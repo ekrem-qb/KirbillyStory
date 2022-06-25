bfff9c260fa207b594c63fab3a322adc514d4e0a
Ekrem Bayram <ekrem.bayram@mail.ru>
2022-06-25 13:27:47 +0300

    Add C++ ConstraintExtension
    Add Blueprint suffixes
    Add Reset Player Pose function

---
+++ Source/KirbillyStory/Private/ConstraintExtension.cpp
@@ -0,0 +1,11 @@
+﻿#include "ConstraintExtension.h"
+
+void UFConstraintExtension::SetConstraintTargetRotation(USkeletalMeshComponent* Mesh, const FName JointName,
+	const FRotator TargetRotator)
+{
+	FConstraintInstance* Joint = Mesh->FindConstraintInstance(JointName);
+	if (Joint != nullptr)
+	{
+		Joint->SetAngularOrientationTarget(TargetRotator.Quaternion());
+	}
+}
---
+++ Source/KirbillyStory/Private/ConstraintExtension.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+#include "ConstraintExtension.generated.h"
+
+UCLASS()
+class UFConstraintExtension final : public UObject
+{
+	GENERATED_BODY()
+public:
+	UFUNCTION(BlueprintCallable, Category="Physics|Components|PhysicsConstraint")
+	static void SetConstraintTargetRotation(USkeletalMeshComponent* Mesh, const FName JointName,
+	                                        const FRotator TargetRotator);
+private:
+	UFConstraintExtension()
+	{
+	};
+};
